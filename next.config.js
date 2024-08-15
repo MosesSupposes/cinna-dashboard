bf1511990c748a3eb3bb165b66a6d3da308f476b
MosesSupposes <mosesasamuel633@gmail.com>
2024-08-15 15:28:01 -0400

    revert next config to its default configuration upon project initialization

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
