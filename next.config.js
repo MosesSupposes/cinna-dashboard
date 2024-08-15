fccf1e4483738c55e284b50e5a0961ec2a586a44
MosesSupposes <mosesasamuel633@gmail.com>
2024-08-15 15:22:48 -0400

    add environment variables to the next config for deployment purposes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+};
 
 module.exports = nextConfig;
